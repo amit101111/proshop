1e3c3bca1d68049f22bd6af7733eb0b2e66e13b7
amit101111 <amit1999cse47@gmail.com>
2022-04-22 14:51:51 +0600

    FrontEnd-22-April

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
-const App = () => {
+function App() {
   return (
-    <>
-      <h1>Welcome to ProShop.</h1>
-    </>
-  );
-};
+    <Router>
+      <Header />
+      <main className='my-3'>
+        <Container>
+          <main>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} />
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </main>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
+}
 
-export default App;
+export default App
